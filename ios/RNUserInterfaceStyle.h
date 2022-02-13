660b8430c61f41775ab760fbfc8bb09aee9f1c40
maruzzing <sseulsseul@gmail.com>
2022-02-13 13:36:08 +0900

    feat: add ios module

--- ios/RNUserInterfaceStyle.h
+++ ios/RNUserInterfaceStyle.h
@@ -1,7 +1,18 @@
 // RNUserInterfaceStyle.h
 
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface RNUserInterfaceStyle : NSObject <RCTBridgeModule>
++ (void)initSystemTheme;
++ (void)setSystemTheme:(NSString *)theme update:(BOOL)update;
++ (NSString *)get:(NSString *)key;
++ (void)set:(NSString *)key value:(NSString *)value;
++ (NSUserDefaults *)getUserDefaults;
 
 @end
+
+NS_ASSUME_NONNULL_END
+
--- ios/RNUserInterfaceStyle.m
+++ ios/RNUserInterfaceStyle.m
@@ -1,16 +1,64 @@
 // RNUserInterfaceStyle.m
 
 #import "RNUserInterfaceStyle.h"
-
+@import UIKit;
 
 @implementation RNUserInterfaceStyle
 
-RCT_EXPORT_MODULE()
++ (NSUserDefaults *)getUserDefaults {
+    NSString *suiteName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"];
+    if(suiteName == nil){
+        return [NSUserDefaults standardUserDefaults];
+    } else {
+        return [[NSUserDefaults alloc] initWithSuiteName:suiteName];
+    }
+}
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
++ (void)set:(NSString *)key value:(NSString *)value {
+    return [[RNUserInterfaceStyle getUserDefaults] setObject:value forKey:key];
+}
+
++ (NSString *)get:(NSString *)key {
+    return [[RNUserInterfaceStyle getUserDefaults] stringForKey:key];
+}
+
++ (void)setSystemTheme:(NSString *)theme update:(BOOL)update{
+  if(update){
+    [RNUserInterfaceStyle set:@"theme" value:theme];
+  }
+  if (@available(iOS 13.0, *)) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      if ([theme isEqual: @"dark"]) {
+        UIApplication.sharedApplication.delegate.window.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
+      } else if ([theme isEqual: @"light"]) {
+        UIApplication.sharedApplication.delegate.window.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+      } else {
+        UIApplication.sharedApplication.delegate.window.overrideUserInterfaceStyle = UIUserInterfaceStyleUnspecified;
+      }
+    });
+  }
+}
+
++ (void)initSystemTheme {
+  NSString *theme = [RNUserInterfaceStyle get:@"theme"];
+  if (theme == nil) {
+    [RNUserInterfaceStyle setSystemTheme:@"default" update:NO];
+  } else {
+    [RNUserInterfaceStyle setSystemTheme:theme update:NO];
+  }
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(setTheme:(NSString *)theme)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  [RNUserInterfaceStyle setSystemTheme:theme update:YES];
+}
+
+RCT_EXPORT_METHOD(getTheme:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+  resolve([RNUserInterfaceStyle get:@"theme"]);
 }
 
 @end
