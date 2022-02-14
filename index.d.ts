8a2746689c848bfbb966c2846d9a79d8c80acf18
maruzzing <sseulsseul@gmail.com>
2022-02-14 21:37:39 +0900

    fix: fix type declare

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
-declare module "react-native-override-user-interface-style" {
-  export type Theme = "default" | "light" | "dark";
+export type Theme = "default" | "light" | "dark";
 
-  export default class RNUserInterfaceStyle {
-    static getTheme(): Promise<Theme | null>;
-    static setTheme(theme: Theme): void;
-  }
+export default class RNUserInterfaceStyle {
+  static getTheme(): Promise<Theme | null>;
+  static setTheme(theme: Theme): void;
 }
