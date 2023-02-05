24da62d6a19a8124a72043ba0b4b2c3bd6cb7d82
Lode Vanhove <lode.vanhove@gmail.com>
2023-02-05 06:59:05 +0100

    fix: a module cannot have multiple default exports (#2)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // main index.js
 
-import { NativeModules } from "react-native";
+import { NativeModules } from 'react-native';
 
 const { RNUserInterfaceStyle } = NativeModules;
 
-export default class RNUserInterfaceStyle {
-  static getTheme = RNUserInterfaceStyle.getTheme();
-  static setTheme = (theme) => RNUserInterfaceStyle.setTheme(theme);
-}
+export default {
+  getTheme: RNUserInterfaceStyle.getTheme,
+  setTheme: RNUserInterfaceStyle.setTheme,
+};
